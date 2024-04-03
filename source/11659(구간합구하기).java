62a871dd51565f382f6d8db31d92ff07a6157327
JEONGJIWON\wldnj <wldnjs9705@gmail.com>
2024-04-03 19:29:00 +0900

    누적합

---
+++ source/11659(구간합구하기).java
@@ -0,0 +1,39 @@
+import java.sql.SQLOutput;
+import java.util.*;
+import java.io.*;
+
+public class Main{
+
+    static int N, M;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    static HashMap<String, String> map = new HashMap<>();
+    static int [] arr;
+    static int [] dp;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N+1];
+        dp  = new int[N+1];
+        st = new StringTokenizer(br.readLine());
+        for(int i=1; i<=N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+            dp[i] = dp[i-1] + arr[i];
+        }
+        for(int i=0; i<M; i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            bw.write(dp[b] - dp[a-1] + "\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+
+
+
+
+}
